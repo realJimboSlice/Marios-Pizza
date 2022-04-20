468201bc6baa06d0bb8a0dfb95099a7aeba15b90
SebastianHeiberg <seba3673@stud.kea.dk>
2022-04-20 11:00:00 +0200

    Main menu added

--- src/com/company/Controller.java
+++ src/com/company/Controller.java
@@ -3,4 +3,25 @@ package com.company;
 public class Controller {
   UserInterface ui = new UserInterface();
 
+  public void mainMenu () {
+
+    boolean loop = true;
+    while (loop) {
+
+      ui.displayMenu();
+      int choice = ui.indputNumber();
+
+      switch (choice){
+        case 1 -> System.out.println("Display the Menu");
+        case 2 -> System.out.println("Add Orders");
+        case 3 -> System.out.println("View Orders");
+        case 4 -> System.out.println("Remove Order");
+        case 5 -> System.out.println("Display Stats");
+        case 10 -> loop = false;
+        default -> System.out.println("Invalid choice");
+      }
+
+    }
+
+  }
 }
