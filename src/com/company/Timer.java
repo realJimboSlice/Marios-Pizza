eb1018770ad9eaf56fb4b646f59d98be4b9be523
realJimboSlice <jimm7493@stud.kea.dk>
2022-04-21 09:17:10 +0200

    Opdateret timeren

--- src/com/company/Timer.java
+++ src/com/company/Timer.java
@@ -1,4 +1,58 @@
 package com.company;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
 public class Timer {
+  static void Klokkeslæt() {
+    long millis = System.currentTimeMillis();
+
+    Calendar calendar = Calendar.getInstance();
+    calendar.setTimeInMillis(millis);
+
+    long hourofday = (calendar.get(Calendar.HOUR_OF_DAY));
+    long minute = calendar.get(Calendar.MINUTE);
+
+    if (hourofday >= 10) {
+      System.out.println(hourofday + ":" + minute);
+    } else {
+      System.out.println("\nButikken er ikke åben endnu");
+    }
+
+    // Timeren viser en time senere (altså hvornår pizzaen skal afhentes (3600000 er en time i millisekunder)
+    Date date = new Date(System.currentTimeMillis()+3600000);
+    String yourDesiredDateValue = new SimpleDateFormat("hh:mm").format(date);
+    System.out.println("\nOrdren skal være klar kl. " + yourDesiredDateValue);
+  }
+
+  static void Nedtælling() throws InterruptedException {
+
+    Scanner scan = new Scanner(System.in);
+    System.out.println("\nHvor lang tid skal der gå før ordren er klar til afhentning (i minutter) ?");
+    // Konverterer input til sekunder
+    int timet= scan.nextInt() * 60;
+    // konverterer sekunder til millisekunder
+    long delay = timet * 1000;
+
+    do
+    {
+      // Variabler - ikke røre
+      int minutes = timet / 60;
+      int seconds = timet % 60;
+      System.out.println(minutes +" minut(ter), " + seconds + " sekund(er)");
+      // Hvor lang tid der går i mellem hvert tick (i millisekunder)
+      // (1000 millisekunder = 1 sekund) (60000 millisekunder = 1 minut) (3600000 millisekunder = 1 time)
+      Thread.sleep(1000);
+      // Hvor mange sekunder der bliver hugget hver gang den ticker
+      timet = timet - 1;
+      // Ikke røre
+      delay = delay - 1000;
+
+    }
+    while (delay != 0);
+    System.out.println("Ordre (nummer) klar til afhentning");
+  }
+
 }
